1af89badecc8b3a1e8711950928b6daf41261eb6
Scott Tran <scotttran506@yahoo.com>
2022-11-03 10:38:12 -0500

    moved implementation to headers

---
+++ include/debugger/debugger.hxx
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "linenoise.h"
+#include "utils.hxx"
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+class Debugger
+{
+public:
+  Debugger(std::string prog_name, pid_t pid)
+    : prog_name_(std::move(prog_name))
+    , pid_(pid)
+  {
+  }
+
+  auto run() const;
+  auto handle_command(const std::string& line) const;
+  auto continue_execution() const;
+
+private:
+  const std::string prog_name_;
+  const pid_t pid_;
+};
+
+auto
+Debugger::continue_execution() const
+{
+  ptrace(PTRACE_CONT, pid_, 0, 0);
+
+  int wait_status;
+  waitpid(pid_, &wait_status, 0);
+}
+
+auto
+Debugger::handle_command(const std::string& line) const
+{
+  auto args = split(line, " ");
+  auto command = args[0];
+
+  if (is_prefix(command, "continue")) {
+    continue_execution();
+  } else {
+    std::cerr << "Unknown command\n";
+  }
+}
+
+auto
+Debugger::run() const
+{
+  int wait_status;
+  waitpid(pid_, &wait_status, 0);
+
+  char* line = nullptr;
+  while ((line = linenoise("debug> ")) != nullptr) {
+    handle_command(line);
+    linenoiseHistoryAdd(line);
+    linenoiseFree(line);
+  }
+}
--- include/debugger/utils.hxx
+++ include/debugger/utils.hxx
@@ -9,3 +9,27 @@ split(const std::string& s, const std::string& delimiter)
 
 auto
 is_prefix(const std::string& s, const std::string& pre) -> bool;
+
+auto
+split(const std::string& s, const std::string& delimiter)
+  -> std::vector<std::string>
+{
+  auto res = std::vector<std::string>{};
+
+  auto start = 0U;
+  auto end = s.find(delimiter);
+
+  while (end != std::string::npos) {
+    res.push_back(s.substr(start, end - start));
+    start = end + delimiter.length();
+    end = s.find(delim, start);
+  }
+
+  return res;
+}
+
+auto
+is_prefix(const std::string& s, const std::string& pre) -> bool
+{
+  return s.compare(0, pre.size(), pre) == 0;
+}
